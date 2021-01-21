79bd7d33b5e1d9a95a66f2d1e1723e7c9647750e
lauro <lauropimentel12@gmail.com>
2021-01-21 16:12:59 -0300

    hello

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ perms = {'key': home+'/ssl/ca.pem',
         
          }
 
-db = MySQLDatabase('peppertools_8187', user='peppertools_8187', passwd='xI36ApTIlYcEk-h6gWfZ', charset='utf8mb4',  host='64531231-4dcd-423f-87d3-b7b0f86d4fc6.peppertools-8187.mysql.dbs.scalingo.com', port=34711)
+db = MySQLDatabase('peppertools_8187', user='peppertools_8187', passwd='xI36ApTIlYcEk-h6gWfZ', charset='utf8mb4',  host='64531231-4dcd-423f-87d3-b7b0f86d4fc6.peppertools-8187.mysql.dbs.scalingo.com', port=34711 or 'peppertools.db')
 
 class Clientes(Model):
     ID= PrimaryKeyField()
