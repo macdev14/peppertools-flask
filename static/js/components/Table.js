3f787bcd1788073245d6142eb56047b76b31a9d2
lauro <lauropimentel12@gmail.com>
2021-03-22 18:27:58 -0300

    Added table process view

--- static/js/components/Table.js
+++ static/js/components/Table.js
@@ -11,28 +11,68 @@ export default class Table extends React.Component
     constructor(props){
         super(props);
         this.state = {
-            processos : []
+            processos : [],
+            production : []
         }
     }
+
+
+ requestOS(){
+    return axios.get(`https://peppertools.herokuapp.com/api/progress/allos`, {headers: {'authorization': localStorage.getItem('auth') }}).then((response) => {
+    response.data.map((val, i) => {
+    return this.requestOSstatus(val, i)
+   })
+    })
+ }
+
+
 
   requestProcesses(){
   let response = axios.get('https://peppertools.herokuapp.com/api/processos', {headers: {'authorization': localStorage.getItem('auth') }}).then((response) => {
   // console.log(response.data)
    response.data.map((val, i) => {
    
-    let arr = [...this.state.processos, {field : val['Nome'], width: 200}]
+    let arr = [...this.state.processos, {field : val['Nome'], width: 180}]
     this.setState({processos: arr})
-    console.log(arr)
     
      
    })
+   this.requestOS()
+ 
+})
+  }
+
+
+ requestOSstatus(nOs, index){
+     
+     let response = axios.get(`https://peppertools.herokuapp.com/api/progress/${nOs}`, {headers: {'authorization': localStorage.getItem('auth') }}).then((response) =>{ return response.data
+     }).then((res)=>
+     { 
+      //let index = ( this.state.processos.findIndex(x => x.field  === res[0]['Nome']) ) + 1
+      //console.log(index) 
+      let arr = [...this.state.production, {id: index, [res[0]['Nome']] : nOs.toString()}]
+      this.setState({production: arr})
+     }
+     )
+     
+  // console.log(response.data)
+   /*response.data.map((val, i) => {
+   
+    let arr = [...this.state.processos, {field : val['Nome'], width: 200}]
+    this.setState({processos: arr})
+    console.log(arr)*
+    
+     */
+   
    
  
-})   
-  }
+
+ }
+
+ 
+
     componentDidMount(){
-       
-     this.requestProcesses()   
+     this.requestProcesses()
         
     }
         
@@ -42,14 +82,16 @@ export default class Table extends React.Component
 
     render(){
         var that = this;
+        console.log(that.state.production);
         return(
-        <DataGrid
-      columns={that.state.processos}
-  rows={[
-    { id: 1, name: 'React' },
-    { id: 2, name: 'Material-UI' },
-  ]}
-    />
+         <div style={{ height: 500, width: '100%', color: '#000000', display: 'flex', justifyContent:'center', alignItems:'center' }}>
+       <DataGrid
+  columns={that.state.processos}
+  rows={that.state.production} components={{
+    Toolbar: CustomToolbar,
+  }} />
+       
+        </div>
         )
         
         }
